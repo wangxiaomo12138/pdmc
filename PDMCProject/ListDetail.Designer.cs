ba1749d55e253b6e2b228d29f770deab7a841158
wangxiaomo <369617725@qq.com>
2021-11-01 08:50:35 +0800

    提交

---
+++ PDMCProject/ListDetail.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace PDMCProject
+{
+    partial class ListDetail
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.category = new System.Windows.Forms.Label();
+            this.from = new System.Windows.Forms.Label();
+            this.url = new System.Windows.Forms.Label();
+            this.author = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.title.Location = new System.Drawing.Point(10, 8);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(47, 14);
+            this.title.TabIndex = 0;
+            this.title.Text = "title";
+            
+            // 
+            // category
+            // 
+            this.category.AutoSize = true;
+            this.category.Location = new System.Drawing.Point(10, 38);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(53, 12);
+            this.category.TabIndex = 1;
+            this.category.Text = "category";
+            // 
+            // from
+            // 
+            this.from.AutoSize = true;
+            this.from.Location = new System.Drawing.Point(112, 37);
+            this.from.Name = "from";
+            this.from.Size = new System.Drawing.Size(29, 12);
+            this.from.TabIndex = 2;
+            this.from.Text = "form";
+            
+            // 
+            // url
+            // 
+            this.url.AutoSize = true;
+            this.url.Location = new System.Drawing.Point(243, 38);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(23, 12);
+            this.url.TabIndex = 3;
+            this.url.Text = "url";
+            // 
+            // author
+            // 
+            this.author.AutoSize = true;
+            this.author.Location = new System.Drawing.Point(406, 8);
+            this.author.Name = "author";
+            this.author.Size = new System.Drawing.Size(41, 12);
+            this.author.TabIndex = 4;
+            this.author.Text = "author";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(90, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "|";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(221, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "|";
+            // 
+            // ListDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.author);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.from);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.title);
+            this.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.Name = "ListDetail";
+            this.Size = new System.Drawing.Size(568, 60);
+            this.Click += new System.EventHandler(this.ListDetail_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label category;
+        private System.Windows.Forms.Label from;
+        private System.Windows.Forms.Label url;
+        private System.Windows.Forms.Label author;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ PDMCProject/ListDetail.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using static PDMCProject.DetailPage;
+
+namespace PDMCProject
+{
+    public partial class ListDetail : UserControl
+    {
+        Microsoft.Office.Tools.CustomTaskPane ctp;
+        public ListDetail()
+        {
+            InitializeComponent();
+        }
+
+        public ListDetail(string title,string author,string category,string from,string url)
+        {
+            InitializeComponent();
+            this.title.Text = title;
+            this.author.Text = author;
+            this.category.Text = category;
+            this.from.Text = from;
+            this.url.Text = url;
+        }
+        private void ListDetail_Click(object sender, EventArgs e)
+        {
+            if (!this.from.Text.Equals("PDMC+"))
+            {
+                MessageBox.Show("暂时无法解析该地址");
+            }
+            else
+            {
+                JObject param = new JObject();
+                param.Add("file_url", this.url.Text);
+                param.Add("hash_code", Globals.ThisAddIn.userInfo);
+                JObject result = HttpClient.Post(Globals.ThisAddIn.detailUrl, param.ToString());
+                string[] split = result.GetValue("code").ToString().Split('_');
+                if ("500".Equals(split[1]))
+                {
+                    MessageBox.Show(result.GetValue("msg").ToString());
+                }
+                if ("400".Equals(split[1]))
+                {
+                    //返回登录超时，采用缓存中的信息后台静默登录；
+                    MessageBox.Show(result.GetValue("msg").ToString());
+                    JObject jb1 = new JObject();
+                    jb1.Add("username", Globals.ThisAddIn.username);
+                    jb1.Add("user_password", Globals.ThisAddIn.user_password);
+                    JObject result1 = HttpClient.Login(Globals.ThisAddIn.loginUrl, jb1.ToString());
+                    string[] split1 = result1.GetValue("code").ToString().Split('_');
+                    if (!"200".Equals(split1[1]))
+                    {
+                        MessageBox.Show(result.GetValue("msg").ToString());
+                        LoginForm loginFrom = new LoginForm();
+                        loginFrom.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show(result.GetValue("msg").ToString());
+                        JObject data = (JObject)result.GetValue("data");
+                        Globals.ThisAddIn.userInfo = data.GetValue("hash_code").ToString();
+                        ListDetail_Click(sender, e);
+                    }
+
+                }
+                if ("200".Equals(split[1]))
+                {
+                    JObject data = JObject.Parse(result.GetValue("data").ToString());
+                    JObject basic = JObject.Parse(data.GetValue("basic_info").ToString());
+                    string fileName = data.GetValue("file_name").ToString().Split('.')[1];
+                    JavaScriptSerializer Serializer = new JavaScriptSerializer();
+                    List<VersionDto> list = Serializer.Deserialize<List<VersionDto>>(data.GetValue("version").ToString());
+                    DetailPage detail = new DetailPage(basic.GetValue("document_name").ToString(),
+                        basic.GetValue("process").ToString(),
+                        basic.GetValue("process_type").ToString(),
+                        list,fileName
+                        );
+                    ctp = Globals.ThisAddIn.CustomTaskPanes.Add(detail, "文档详情");
+                    ctp.Visible = true;
+                }
+            }
+        }
+    }
+}
